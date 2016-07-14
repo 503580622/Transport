c1a369c2f81caae4efeafa754bcd59d7ab493371
503580622 <503580622@qq.com>
2016-07-14 13:40:17 +0800

    修改网络配置链接地址

--- app/src/main/java/com/jiahelogistic/config/NetConfig.java
+++ app/src/main/java/com/jiahelogistic/config/NetConfig.java
@@ -8,31 +8,11 @@ public class NetConfig {
 	/**
 	 * 连接根地址
 	 */
-	public static final String URLROOT = "http://10.0.0.2";
+	public static final String URLROOT = "http://10.0.2.2";
 
 	/**
 	 * 异常文件上传地址
 	 */
-	public static final String CRASH_FILE_UPLOAD_URL = "http://10.0.0.2/upload.php";
-
-
-//	/**
-//	 * 单个实例
-//	 */
-//	private static NetConfig config = null;
-//
-//	private NetConfig() {
-//	}
-//
-//	/**
-//	 * 获取网络配置实例
-//	 * @return 配置对象
-//	 */
-//	public static NetConfig getInstance() {
-//		if (config == null) {
-//			config = new NetConfig();
-//		}
-//		return config;
-//	}
+	public static final String CRASH_FILE_UPLOAD_URL = URLROOT + "/upload.php";
 
 }
